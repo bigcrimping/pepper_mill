7677f9274419552a770b02d4e2d0cb4757f52c46
slai-mark <mark@saliencelabs.ai>
2024-11-12 09:42:13 +0000

    minor tidy up

--- micropython/main.py
+++ micropython/main.py
@@ -5,24 +5,15 @@ from ssd1306 import SSD1306_I2C
 from rotary_irq_rp2 import RotaryIRQ
 from hx711 import *
  
+# stepper motor
 dir_pin = Pin(18, Pin.OUT)
 step_pin = Pin(19, Pin.OUT)
 en_pin = Pin(20, Pin.OUT)
+en_pin.value(1)
+dir_pin.value(1)
+
+# scales
 hx = hx711(Pin(12), Pin(13))
-i2c=I2C(1,sda=Pin(2), scl=Pin(3), freq=400000)
-button = Pin(6, Pin.IN, Pin.PULL_UP)
-
-r = RotaryIRQ(pin_num_clk=4, 
-              pin_num_dt=5, 
-              min_val=0, 
-              max_val=2, 
-              reverse=True, 
-              range_mode=RotaryIRQ.RANGE_WRAP)
-
-
-display = SSD1306_I2C(128, 64, i2c)
-display.fill(0)
-
 hx.set_power(hx711.power.pwr_up)
 hx.set_gain(hx711.gain.gain_128)
 hx.set_power(hx711.power.pwr_down)
@@ -30,16 +21,21 @@ hx711.wait_power_down()
 hx.set_power(hx711.power.pwr_up)
 hx711.wait_settle(hx711.rate.rate_10)
 
-en_pin.value(1)
-dir_pin.value(1)
+# display
+i2c=I2C(1,sda=Pin(2), scl=Pin(3), freq=400000)
+display = SSD1306_I2C(128, 64, i2c)
+display.fill(0)
 
-display.text("PepperMill 1.0",0,0)
-display.text("How much pepper?",0,17)
-display.text("",0,27)
-display.text("A little bit  <-",0,37)
-display.text("Lots please",0,47)
-display.text("All of it!!",0,57)
-display.show()
+# button
+button = Pin(6, Pin.IN, Pin.PULL_UP)
+
+# knob
+r = RotaryIRQ(pin_num_clk=4, 
+              pin_num_dt=5, 
+              min_val=0, 
+              max_val=2, 
+              reverse=True, 
+              range_mode=RotaryIRQ.RANGE_WRAP)
 
 def rotate(steps):
     en_pin.low()
@@ -51,37 +47,18 @@ def rotate(steps):
     en_pin.high()
     
 def update_display(val_new):
-
-    if (val_new == 0):
+        options = ["A little bit", "Lots please", "All of it!!"]
+        if val_new < len(options):
+            options[val_new] = options[val_new] + " <--"
         display.fill(0)
         display.text("PepperMill 1.0",0,0)
         display.text("How much pepper?",0,17)
         display.text("",0,27)
-        display.text("A little bit  <-",0,37)
-        display.text("Lots please",0,47)
-        display.text("All of it!!",0,57)
-        display.show()
-    elif (val_new == 1):
-        display.fill(0)
-        display.text("PepperMill 1.0",0,0)
-        display.text("How much pepper?",0,17)
-        display.text("",0,27)
-        display.text("A little bit",0,37)
-        display.text("Lots please   <-",0,47)
-        display.text("All of it!!",0,57)
-        display.show()
-    elif (val_new == 2):
-        display.fill(0)
-        display.text("PepperMill 1.0",0,0)
-        display.text("How much pepper?",0,17)
-        display.text("",0,27)
-        display.text("A little bit",0,37)
-        display.text("Lots please",0,47)
-        display.text("All of it!!   <-",0,57)
+        for i in range(len(options)):
+            display.text(options[i],0,37+(10*i))
         display.show()
         
 def pepper_dispense(val_new):
-
     if (val_new == 0):
         rotate(10000)
     elif (val_new == 1):
@@ -91,10 +68,12 @@ def pepper_dispense(val_new):
 
 
 def loop():
-    
     val_old = r.value()
     debounce_time=10
-    
+
+    # initialise the display
+    update_display(val_old)
+
     while True:
     
         val_new = r.value()
@@ -109,7 +88,8 @@ def loop():
             r._disable_dt_irq()
             debounce_time=time.ticks_ms()
             #print("Button Pressed")
-            #pepper_dispense(val_new)
+            # dispense the pepper!
+            pepper_dispense(val_new)
             r._enable_clk_irq()
             r._enable_dt_irq()
     
