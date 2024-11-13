ebf4aee1a6d2604b5cf2c520a7dd01d88c948517
bigcrimping <74270551+bigcrimping@users.noreply.github.com>
2024-11-13 21:45:57 +0000

    Added scales measurment
    Added function to measure the dispensed amount in mg and show to user

--- micropython/main.py
+++ micropython/main.py
@@ -11,6 +11,8 @@ en_pin = Pin(20, Pin.OUT)
 hx = hx711(Pin(12), Pin(13))
 i2c=I2C(1,sda=Pin(2), scl=Pin(3), freq=400000)
 button = Pin(6, Pin.IN, Pin.PULL_UP)
+count2mg = 18
+
 
 r = RotaryIRQ(pin_num_clk=4, 
               pin_num_dt=5, 
@@ -21,7 +23,6 @@ r = RotaryIRQ(pin_num_clk=4,
 
 
 display = SSD1306_I2C(128, 64, i2c)
-display.fill(0)
 
 hx.set_power(hx711.power.pwr_up)
 hx.set_gain(hx711.gain.gain_128)
@@ -33,26 +34,29 @@ hx711.wait_settle(hx711.rate.rate_10)
 en_pin.value(1)
 dir_pin.value(1)
 
-display.text("PepperMill 1.0",0,0)
-display.text("How much pepper?",0,17)
-display.text("",0,27)
-display.text("A little bit  <-",0,37)
-display.text("Lots please",0,47)
-display.text("All of it!!",0,57)
-display.show()
-
 def rotate(steps):
     en_pin.low()
     for i in range(steps):
         step_pin.high()
-        utime.sleep_us(50)
+        utime.sleep_us(70)
         step_pin.low()
-        utime.sleep_us(50)
+        utime.sleep_us(70)
     en_pin.high()
-    
-def update_display(val_new):
 
-    if (val_new == 0):
+def display_mg(mg):
+    
+    display.fill(0)
+    display.text("",0,0)
+    display.text("",0,17)
+    display.text("   Dispensed",0,27)
+    display.text("   " + mg + " mg",0,37)
+    display.text("",0,47)
+    display.text("",0,57)
+    display.show()
+    utime.sleep(4)
+
+def update_display(display_val):
+    if (display_val == 0):
         display.fill(0)
         display.text("PepperMill 1.0",0,0)
         display.text("How much pepper?",0,17)
@@ -61,7 +65,7 @@ def update_display(val_new):
         display.text("Lots please",0,47)
         display.text("All of it!!",0,57)
         display.show()
-    elif (val_new == 1):
+    elif (display_val == 1):
         display.fill(0)
         display.text("PepperMill 1.0",0,0)
         display.text("How much pepper?",0,17)
@@ -70,7 +74,7 @@ def update_display(val_new):
         display.text("Lots please   <-",0,47)
         display.text("All of it!!",0,57)
         display.show()
-    elif (val_new == 2):
+    elif (display_val == 2):
         display.fill(0)
         display.text("PepperMill 1.0",0,0)
         display.text("How much pepper?",0,17)
@@ -79,20 +83,45 @@ def update_display(val_new):
         display.text("Lots please",0,47)
         display.text("All of it!!   <-",0,57)
         display.show()
-        
-def pepper_dispense(val_new):
+    elif (display_val == 3):
+        display.fill(0)
+        display.text("",0,0)
+        display.text("",0,17)
+        display.text("   Dispensing",0,27)
+        display.text("",0,37)
+        display.text("",0,47)
+        display.text("",0,57)
+        display.show()
 
+def read_weight(readings):
+    total = 0
+    for i in range(readings):
+        total = total + hx.get_value()
+        utime.sleep_us(5000)
+        
+    total = total/readings
+    return total
+            
+def pepper_dispense(val_new):
+    update_display(3)
+    before = read_weight(10)
     if (val_new == 0):
         rotate(10000)
     elif (val_new == 1):
         rotate(20000)
     elif (val_new == 2):
         rotate(60000)
+    after = read_weight(10)
+    measure = "{:.2f}".format((after-before)/count2mg)
+    print(measure)
+    display_mg(measure)
+    update_display(val_new)
 
 
 def loop():
     
     val_old = r.value()
+    update_display(0)
     debounce_time=10
     
     while True:
@@ -108,8 +137,8 @@ def loop():
             r._disable_clk_irq()
             r._disable_dt_irq()
             debounce_time=time.ticks_ms()
-            #print("Button Pressed")
-            #pepper_dispense(val_new)
+            print("Button Pressed")
+            pepper_dispense(val_new)
             r._enable_clk_irq()
             r._enable_dt_irq()
     
